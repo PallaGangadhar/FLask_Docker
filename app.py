ff06c645522ef893a33a20ac1f0c2da6889729cb
Ganagdhar <ganga11497@gmail.com>
2024-11-20 17:13:13 +0530

    new

--- app.py
+++ app.py
@@ -12,35 +12,8 @@ def name():
 
 @app.route('/about', methods=['GET'])
 def about():
-    print("Hii fgdf")
-    print("Hii fgdf")
-    print("Hii fgdf dfdsfdfdf")
-    print("Hii dfdsfd fgfdg")
-    print("Hii hlloooo fgfdgfdgdfg")
-    print(" sdfkdk  kfjgk dfkjfdk gkdf gkdfj kgjdfkgjdfk")
-    print("Hii fgdf")
-    print("Hii fgdf")
-    print("Hii fgdf dfdsfdfdf")
-    print("Hii fgfdg")
-    print("Hii hlloooo sddfdsf fgfdgfdgdfg")
-    print(" sdfkdk  kfjgk dfkjfdk gkdf gkdfj kgjdfkgjdfk")
-    print("Hii fgdf")
-    print("Hii fgdf")
-    print("Hii fgdf dfdsfdfdf")
-    print("Hii fgfdg")
-    print("Hii hlloooo fgfdgfdgdfg")
-    print(" sdfkdk  kfjgk dfkjfdk gkdf gkdfj kgjdfkgjdfk")
     return "Hello GANGDADHAR Palla"
 
-@app.route('/login', methods=['GET'])
-def login():
-    print("Hii fgdf")
-    print("Hii fgdf")
-    print("Hii fgdf dfdsfdfdf")
-    print("Hii fgfdg")
-    print("Hii hlloooo fgfdgfdgdfg")
-    print(" sdfkdk  kfjgk dfkjfdk gkdf gkdfj kgjdfkgjdfk")
-    return "Hello GANGDADHAR Palla"
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000, host="0.0.0.0")
