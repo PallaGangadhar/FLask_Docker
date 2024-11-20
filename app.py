2855b16b59fa6e0c4f7f1aded8bcd9b3b0133606
Ganagdhar <ganga11497@gmail.com>
2024-11-20 15:50:59 +0530

    add name() in app.py

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return "Hello DAMAN"
 
+@app.route('/name', methods=['GET'])
+def name():
+    return "Hello GANGDADHAR"
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000, host="0.0.0.0")
